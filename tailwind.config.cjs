4fb53dd22a3b2f94868db0104d1e6a2b697fb155
Aly Mama <52185940+alywheng27@users.noreply.github.com>
2023-05-18 21:19:03 +0800

    Add Header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,42 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				merchant: ['Merchant', 'sans-serif'],
+				// Add your custom font families here
+			},
+			colors: {
+				/* Primary */
+				'msu-maroon': '#75140C;',
+				'msu-deep-ocean': '#0A1C37',
+				'msu-gold': '#F9D92B',
+
+				/* Secondary/Light */
+				'msu-rusty-red': '#8C3F38',
+				'msu-shallow-ocean': '#365583',
+				'msu-golden-fog': '#FBE776',
+
+				/* Secondary/Dark */
+				'msu-crimson-ground': '#420C07',
+				'msu-dark-ocean': '#061121',
+				'msu-hidden-gold': '#C7AE22',
+
+				/* Mono/Light */
+				'white-pure': '#FFFFFF',
+				'silver': '#DDDDDD',
+				'titanium': '#BBBBBB',
+
+				/* Mono/Dark */
+				'black-pure': '#000000',
+				'charcoal': '#222222',
+				'graphite': '#444444',
+			},
+			linearGradients: {
+				'blue-purple': ['to right', '#4F3A72', '#8A3097'],
+				'green-yellow': ['45deg', '#00BFA6', '#F9A826'],
+			},
+		},
 	},
 	plugins: [],
 }
