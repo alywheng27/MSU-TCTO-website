c858a6d5524a3d8c8a47d9d83b55d07ac449b661
Aly Mama <52185940+alywheng27@users.noreply.github.com>
2023-05-25 16:10:40 +0800

    Add Articles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,9 @@ const Navbar = (props) => {
 				: (path == '/offices') ? 'hero-office-img'
 				: (path == '/publications') ? 'hero-publication-img'
 				: (path == '/campus') ? 'hero-campus-img'
-				: 'hero-main-img'
+				: (path == '/academic-calendar') ? ''
+				: (path == '/articles') ? 'hero-main-img pb-[980px]'
+				: 'hero-main-img xl:pb-[215px] xs:pb-[99px]'
 
   return (
     <>
@@ -38,27 +40,31 @@ const Navbar = (props) => {
 				<button className="headings h6 xl:hidden xs:block" onClick={toggleNavbar}>MENU</button>
 			</div>
 		</div>
-        <div className={bg + ` relative xl:pb-[215px] xs:pb-[99px]`}>
-			<div className={`xs:bg-msu-deep-ocean xl:bg-transparent xl:bg-gradient-ocean flex xl:flex-row xs:flex-col justify-between items-center 3xl:h-[80px] xl-[60px] text-white-pure xl:px-[12.5%] xs:px-[3.85%] nav ${isOpen && 'active'}`}>
-				<div className="flex 3xl:flex-row xl:flex-row sm:flex-col xs:flex-col xl:gap-20 xl:text-left xs:text-center paragraph p2 w-full">
-					<a href="/admissions" className='py-[15px]'>Admissions</a>
-					<a href="/programs" className='py-[15px]'>Programs</a>
-					<a href="/offices" className='py-[15px]'>Offices</a>
-					<a href="/publications" className='py-[15px]'>Publication</a>
-					<a href="/campus#" className='py-[15px]'>The Campus</a>
-				</div>
-				<div className="relative xl:mt-0 xs:mt-[15px] xl:w-fit xs:w-full">
-					<input type="search" name="" id="" className="bg-transparent border rounded-[20px] 3xl:w-[360px] xl:w-[240px] xs:w-full h-10 pl-12 pr-4 text-center" />
-					<img src="/images/search.png" alt="" className="absolute top-[6px] left-3" />
-				</div>
+        <div className={bg + ` relative`}>
+			<div className='relative'>
+				<div className={`xs:bg-msu-deep-ocean xl:bg-transparent xl:bg-gradient-ocean flex xl:flex-row xs:flex-col justify-between items-center 3xl:h-[80px] xl-[60px] text-white-pure xl:px-[12.5%] xs:px-[3.85%] nav ${isOpen && 'active'}`}>
+					<div className="flex 3xl:flex-row xl:flex-row sm:flex-col xs:flex-col xl:gap-20 xl:text-left xs:text-center paragraph p2 w-full">
+						<a href="/admissions" className='py-[15px]'>Admissions</a>
+						<a href="/programs" className='py-[15px]'>Programs</a>
+						<a href="/offices" className='py-[15px]'>Offices</a>
+						<a href="/publications" className='py-[15px]'>Publication</a>
+						<a href="/campus#" className='py-[15px]'>The Campus</a>
+					</div>
+					<div className="relative xl:mt-0 xs:mt-[15px] xl:w-fit xs:w-full">
+						<input type="search" name="" id="" className="bg-transparent border rounded-[20px] 3xl:w-[360px] xl:w-[240px] xs:w-full h-10 pl-12 pr-4 text-center" />
+						<img src="/images/search.png" alt="" className="absolute top-[6px] left-3" />
+					</div>
+				</div>				
 			</div>
-            <div className='bg-gradient-ocean h-[60px] xs:block xl:hidden'></div>
+			
+            <div className='bg-gradient-ocean h-[60px] xl:hidden xs:block'></div>
 			{
 				(path == '/admissions') ? <HeroAdmissionHeading />
 				: (path == '/programs') ? <HeroProgramHeading />
 				: (path == '/offices') ? <HeroOfficeHeading />
 				: (path == '/publications') ? <HeroPublicationHeading />
 				: (path == '/campus') ? <HeroCampusHeading />
+				: (path == '/academic-calendar' || path == '/articles') ? ''
 				: <HeroMainHeading />
 			}
 			
