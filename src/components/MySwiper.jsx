b154a1a29a34d3a6148e4b4db8bb60c7b933756b
Aly Mama <52185940+alywheng27@users.noreply.github.com>
2023-08-25 14:28:34 +0800

    Make swiper center

--- src/components/MySwiper.jsx
+++ src/components/MySwiper.jsx
@@ -24,11 +24,10 @@ export default function MySwiper() {
     loop={true}
     fade = {true}
     >
-    <div class="flex flex-col gap-[20px] mb-[120px] justify-center w-full">
-        <div  class="flex xl:flex-row xs:flex-row xs:gap-[20px] justify-center xl:hidden xs:block swiper-wrapper">
+
                     
-        <SwiperSlide>
-            <div id="cas" class="msu-drop-white-shadow xs:w-[460px] xs:h-[658px] xs:mx-[15px] xs:mb-[20px] swiper-slide">
+        <SwiperSlide className='flex justify-center'>
+            <div id="cas" class="msu-drop-white-shadow xs:w-[460px] xs:h-[658px] xs:mb-[20px] swiper-slide">
                 <div class="flex xs:mx-[75px] xs:pt-[61px] justify-center">
                     <img src="/images/College Logo/LOGO CAS.png" alt="" class="xs:w-[242px] xs:h-[242px]"></img>
                 </div>
@@ -46,8 +45,8 @@ export default function MySwiper() {
             </div>
 
         </SwiperSlide>
-        <SwiperSlide>
-            <div id="cias" class="msu-drop-white-shadow xs:w-[460px] xs:h-[658px] xs:mx-[15px] xs:mb-[20px] swiper-slide">
+        <SwiperSlide className='flex justify-center'>
+            <div id="cias" class="msu-drop-white-shadow xs:w-[460px] xs:h-[658px] xs:mb-[20px] swiper-slide">
                 <div class="flex xs:mx-[75px] xs:pt-[61px] justify-center">
                     <img src="/images/College Logo/LOGO CIAS.png" alt="" class="xs:w-[242px] xs:h-[242px]"></img>
                 </div>
@@ -64,9 +63,9 @@ export default function MySwiper() {
                 </div>
             </div>
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className='flex justify-center'>
 
-            <div id="coed" class="msu-drop-white-shadow xs:w-[460px] xs:h-[658px] xs:mx-[15px] xs:mb-[20px] swiper-slide">
+            <div id="coed" class="msu-drop-white-shadow xs:w-[460px] xs:h-[658px] xs:mb-[20px] swiper-slide">
                 <div class="flex xs:mx-[75px] xs:pt-[61px] justify-center">
                     <img src="/images/College Logo/LOGO COED.png" alt="" class="xs:w-[242px] xs:h-[242px]"></img>
                 </div>
@@ -84,8 +83,8 @@ export default function MySwiper() {
             </div>
         </SwiperSlide>
 
-        <SwiperSlide>
-            <div id="cof" class="msu-drop-white-shadow xs:w-[460px] xs:h-[658px] xs:mx-[15px]  xs:mb-[20px]">
+        <SwiperSlide className='flex justify-center'>
+            <div id="cof" class="msu-drop-white-shadow xs:w-[460px] xs:h-[658px] xs:mb-[20px] swiper-slide">
                 <div class="flex xs:mx-[75px] xs:pt-[61px] justify-center">
                     <img src="/images/College Logo/LOGO COF.png" alt="" class="xs:w-[242px] xs:h-[242px]"></img>
                 </div>
@@ -103,9 +102,9 @@ export default function MySwiper() {
             </div>
 
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className='flex justify-center'>
 
-            <div id="iict" class="msu-drop-white-shadow xs:w-[460px] xs:h-[658px] xs:mx-[15px]  xs:mb-[20px]">
+            <div id="iict" class="msu-drop-white-shadow xs:w-[460px] xs:h-[658px]  xs:mb-[20px]">
                 <div class="flex xs:mx-[75px] xs:pt-[61px] justify-center">
                     <img src="/images/College Logo/LOGO IICT.png" alt="" class="xs:w-[242px] xs:h-[242px]"></img>
                 </div>
@@ -123,9 +122,9 @@ export default function MySwiper() {
             </div>
 
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className='flex justify-center'>
 
-            <div id="ioes" class="msu-drop-white-shadow xs:w-[460px] xs:h-[658px] xs:mx-[15px]  xs:mb-[20px]">
+            <div id="ioes" class="msu-drop-white-shadow xs:w-[460px] xs:h-[658px] xs:mb-[20px]">
                 <div class="flex xs:mx-[75px] xs:pt-[61px] justify-center">
                     <img src="/images/College Logo/LOGO IOES.png" alt="" class="xs:w-[242px] xs:h-[242px]"></img>
                 </div>
@@ -143,9 +142,7 @@ export default function MySwiper() {
             </div>
 
         </SwiperSlide>
-        
-        </div>
-    </div>
+
     </Swiper>
 
   );
