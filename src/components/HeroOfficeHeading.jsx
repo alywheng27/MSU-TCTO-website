224945ff8854bfab16a64f4fb2fa2818ecc31bac
Aly Mama <52185940+alywheng27@users.noreply.github.com>
2023-09-06 18:17:36 +0800

    Fix Merge error admission

--- src/components/HeroOfficeHeading.jsx
+++ src/components/HeroOfficeHeading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeroOfficeHeading = () => {
   return (
     <div className="relative z-10 xl:bottom-[250px] xs:bottom-[79px] h-[80px] xl:mx-[12.5%] xs:mx-[3.85%]">
-      <h2 className="headings xl:h2 xs:h5 text-center text-black-pure">
+      <h2 className="headings xl:h2 xs:h5 text-center text-white-pure">
         Offices
       </h2>
             <p class="paragraph p2 py-[100px] text-titanium xs:hidden xl:block"><a href="/">Home</a> / The Campus</p>
