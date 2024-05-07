f0464eef436a5fb37ef857ebaa7eca8537ff74e2
Micho Robledo <michorobledo25@gmail.com>
2024-05-07 13:03:30 +0800

    Newly add branch

--- src/components/HeroIICTHeading.jsx
+++ src/components/HeroIICTHeading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeroIICTHeading = () => {
   return (
     <div className="relative z-10 xl:bottom-[250px] xs:bottom-[79px] h-[164px] xl:mx-[12.5%] xs:mx-[3.85%]">
-      <p className="paragraph xl:p2 xs:p3 xl:py-[220px] xs:py-[60px] text-silver"><a href="/">Home</a> / Programs / Institute of Information and Communications Technology</p>
+      {/* <p className="paragraph xl:p2 xs:p3 xl:py-[220px] xs:py-[60px] text-silver"><a href="/">Home</a> / Programs / Institute of Information and Communications Technology</p> */}
     </div>
   )
 }
--- src/components/HeroOfficeHeading.jsx
+++ src/components/HeroOfficeHeading.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const HeroOfficeHeading = () => {
   return (
     <div className="relative z-10 xl:bottom-[250px] xs:bottom-[79px] h-[80px] xl:mx-[12.5%] xs:mx-[3.85%]">
-      <h2 className="headings xl:h2 xs:h5 text-center text-white-pure">
+      {/* <h2 className="headings xl:h2 xs:h5 text-center text-white-pure">
         Offices
       </h2>
-            <p className="paragraph p2 py-[100px] text-titanium xs:hidden xl:block"><a href="/">Home</a> / The Campus</p>
+            <p className="paragraph p2 py-[100px] text-titanium xs:hidden xl:block"><a href="/">Home</a> / The Campus</p> */}
     </div>
   )
 }
