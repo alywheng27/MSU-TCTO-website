38bb4a28cc149e9f39a709c20c3a055a4dc1ffd0
Aly Mama <52185940+alywheng27@users.noreply.github.com>
2024-05-07 11:45:01 +0800

    Fix navbar

--- src/components/HeroAdmissionHeading.jsx
+++ src/components/HeroAdmissionHeading.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const HeroAdmissionHeading = () => {
   return (
     <div className="relative z-0 xl:bottom-[250px] xs:bottom-[79px] h-[80px] xl:mx-[12.5%] xs:mx-[3.85%]">
-      <h2 className="headings xl:h2 xs:h5 text-center">
+      {/* <h2 className="headings xl:h2 xs:h5 text-center">
         Admissions
       </h2>
       
-        <p className="paragraph p2 pt-[100px] text-silver xl:block xs:hidden"><a href="/">Home</a> / Addmissions</p>
+        <p className="paragraph p2 pt-[100px] text-silver xl:block xs:hidden"><a href="/">Home</a> / Addmissions</p> */}
     </div>
   )
 }
